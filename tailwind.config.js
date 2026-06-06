3193e142feb5ce132cd59d7620c797c7d15df25e
DnBright <dnbright000@gmail.com>
2026-06-06 08:35:24 +0700

    “update”

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,40 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Plus Jakarta Sans', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                brand: {
+                    50: '#f0fdfa',
+                    100: '#ccfbf1',
+                    400: '#2dd4bf',
+                    500: '#14b8a6', 
+                    600: '#0d9488',
+                    900: '#134e4a',
+                    950: '#042f2e',
+                },
+                dark: {
+                    900: '#0f172a',
+                    950: '#020617',
+                }
+            },
+            animation: {
+                'blob': 'blob 7s infinite',
+                'marquee': 'marquee 25s linear infinite',
+                'spin-slow': 'spin 15s linear infinite',
+            },
+            keyframes: {
+                blob: {
+                    '0%': { transform: 'translate(0px, 0px) scale(1)' },
+                    '33%': { transform: 'translate(30px, -50px) scale(1.1)' },
+                    '66%': { transform: 'translate(-20px, 20px) scale(0.9)' },
+                    '100%': { transform: 'translate(0px, 0px) scale(1)' },
+                },
+                marquee: {
+                    '0%': { transform: 'translateX(0%)' },
+                    '100%': { transform: 'translateX(-100%)' },
+                }
+            }
         },
     },
 
